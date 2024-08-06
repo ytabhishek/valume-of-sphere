2fc4ac37f48bb25837a5bdb6e7b2b2ddf89ef2d0
Abhishek Pratap Singh <ytabhishek721@gmail.com>
2024-08-06 14:33:11 +0530

    Add files via upload

---
+++ valuem_of_sphere.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class valuem_of_sphere {
+    public static void main(String[] args) {
+        double r = 5;
+        double pai = 3.1415;
+        double v = 1.3333 * pai * r * r * r;
+        System.out.println("The valume of sphere is : " +v);
+    }
+}
